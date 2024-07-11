cb3e70644d78f63c0819666b86cc64527664921a
cbhcbhcbh <15862528607@163.com>
2024-07-11 06:37:00 +0000

    setup project

---
+++ lib/data.ts
@@ -0,0 +1,53 @@
+import React from "react";
+import { FaPython, FaReact } from "react-icons/fa";
+import { FaVuejs } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+
+export const links = [
+    {
+        name: "Home",
+        hash: "#home",
+    },
+    {
+        name: "About",
+        hash: "#about",
+    },
+    {
+        name: "Projects",
+        hash: "#projects",
+    },
+    {
+        name: "Skills",
+        hash: "#skills",
+    },
+    {
+        name: "Experiences",
+        hash: "#experience",
+    },
+    {
+        name: "Contact",
+        hash: "#contact",
+    },
+] as const;
+
+
+export const skillsData = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "TypeScript",
+    "React",
+    "Next",
+    "Framer Motion",
+    "d3",
+    "UI/UX",
+    "Tailwind",
+    "Prisma",
+    "Python",
+    "OpenCV",
+    "Numpy",
+    "Pandas",
+    "PostgreSQL",
+    "Git",
+    "Github",
+] 
